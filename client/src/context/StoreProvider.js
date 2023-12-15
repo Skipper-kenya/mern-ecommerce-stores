8418f605a1629f354c7d22bc3e1da542300eb970
Andrew Kimwetich <kimwetichandrew@gmail.com>
2023-12-15 19:13:19 +0300

    comment

--- client/src/context/StoreProvider.js
+++ client/src/context/StoreProvider.js
@@ -59,7 +59,7 @@ const StoreProvider = ({ children }) => {
     };
     fetchItems();
   }, []);
-
+//provi
   const addToCart = (id) => {
     setCart((prev) => ({ ...prev, [id]: prev[id] + 1 }));
     setDeliveryItems((prev) => ({ ...prev, [id]: prev[id] + 1 }));
