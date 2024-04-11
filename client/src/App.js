a83fb39189f590a70a7b53ede32a5c7be0dad17c
Andrew Kimwetich <kimwetichandrew@gmail.com>
2024-04-11 21:38:00 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+//
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Cart from "./pages/cart/Cart";
